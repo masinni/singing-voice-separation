2c18b512240fda01d75f1f4e59b76183c99f7eb3
masinni <marilenasinni@gmail.com>
2022-11-14 15:32:10 +0200

    plot spectrogram documentation

--- plot_spectrograms.py
+++ plot_spectrograms.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import librosa.display
 
+# Extract spectrogram for mixed signal
 scale, sr = librosa.load('sample/original_sources/ORIG_mono/yifen_2_11.wav')
 FRAME_SIZE = 1024
 HOP_SIZE = 256
@@ -9,6 +10,7 @@ S_scale = librosa.stft(scale, n_fft=FRAME_SIZE, hop_length=HOP_SIZE)
 Y_scale = np.abs(S_scale) ** 2
 Y_log_scale = librosa.power_to_db(Y_scale)
 
+# Extract spectrogram for singing voice
 scalev, sr = librosa.load('sample/separated_rpca/results_for_l=1.0/vocals/yifen_2_11.wav')
 FRAME_SIZE = 1024
 HOP_SIZE = 256
@@ -16,6 +18,7 @@ S_scalev = librosa.stft(scalev, n_fft=FRAME_SIZE, hop_length=HOP_SIZE)
 Y_scalev = np.abs(S_scalev) ** 2
 Y_log_scale_v = librosa.power_to_db(Y_scalev)
 
+# Extract spectrogram fro music accompaniment
 scaleb, sr = librosa.load('sample/separated_rpca/results_for_l=1.0/background/yifen_2_11.wav')
 FRAME_SIZE = 1024
 HOP_SIZE = 256
@@ -33,6 +36,7 @@ def plot_spectrogram(Y, sr, hop_length, y_axis="linear"):
                              y_axis=y_axis)
     plt.colorbar(format="%+2.f")
 
+# Plot spectrograms
 sr = 16000
 plot_spectrogram(Y_log_scale, sr, HOP_SIZE, y_axis="log")
 plot_spectrogram(Y_log_scale_v, sr, HOP_SIZE, y_axis="log")
